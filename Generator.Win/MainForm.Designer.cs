7b7c1ac67f8416294330c74aba9261a6ed8d7ce7
carlosvalladolid <lachoman@hotmail.com>
2014-11-04 09:47:33 -0600

    Controles nuevos y funcionalidad básica

--- Generator.Win/MainForm.Designer.cs
+++ Generator.Win/MainForm.Designer.cs
@@ -28,46 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.MenuBar = new System.Windows.Forms.MenuStrip();
-            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.FileMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.FileExitMenu = new System.Windows.Forms.ToolStripMenuItem();
             this.StatusBar = new System.Windows.Forms.StatusStrip();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.label5 = new System.Windows.Forms.Label();
             this.ServerBox = new System.Windows.Forms.TextBox();
             this.DisconnectButton = new System.Windows.Forms.Button();
-            this.ConnectionButton = new System.Windows.Forms.Button();
+            this.ConnectButton = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.PasswordBox = new System.Windows.Forms.TextBox();
             this.UserBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.DatabaseCombo = new System.Windows.Forms.ComboBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.ColumnGrid = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.MenuBar.SuspendLayout();
             this.groupBox4.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ColumnGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // MenuBar
             // 
             this.MenuBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.archivoToolStripMenuItem});
+            this.FileMenu});
             this.MenuBar.Location = new System.Drawing.Point(0, 0);
             this.MenuBar.Name = "MenuBar";
             this.MenuBar.Size = new System.Drawing.Size(835, 24);
             this.MenuBar.TabIndex = 0;
             this.MenuBar.Text = "menuStrip1";
             // 
-            // archivoToolStripMenuItem
+            // FileMenu
             // 
-            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.salirToolStripMenuItem});
-            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
-            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
-            this.archivoToolStripMenuItem.Text = "Archivo";
+            this.FileMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileExitMenu});
+            this.FileMenu.Name = "FileMenu";
+            this.FileMenu.Size = new System.Drawing.Size(60, 20);
+            this.FileMenu.Text = "Archivo";
             // 
-            // salirToolStripMenuItem
+            // FileExitMenu
             // 
-            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
-            this.salirToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.salirToolStripMenuItem.Text = "Salir";
+            this.FileExitMenu.Name = "FileExitMenu";
+            this.FileExitMenu.Size = new System.Drawing.Size(152, 22);
+            this.FileExitMenu.Text = "Salir";
+            this.FileExitMenu.Click += new System.EventHandler(this.FileExitMenu_Click);
             // 
             // StatusBar
             // 
@@ -82,12 +94,12 @@
             this.groupBox4.Controls.Add(this.label5);
             this.groupBox4.Controls.Add(this.ServerBox);
             this.groupBox4.Controls.Add(this.DisconnectButton);
-            this.groupBox4.Controls.Add(this.ConnectionButton);
+            this.groupBox4.Controls.Add(this.ConnectButton);
             this.groupBox4.Controls.Add(this.label3);
             this.groupBox4.Controls.Add(this.label2);
             this.groupBox4.Controls.Add(this.PasswordBox);
             this.groupBox4.Controls.Add(this.UserBox);
-            this.groupBox4.Location = new System.Drawing.Point(27, 43);
+            this.groupBox4.Location = new System.Drawing.Point(15, 72);
             this.groupBox4.Name = "groupBox4";
             this.groupBox4.Size = new System.Drawing.Size(273, 149);
             this.groupBox4.TabIndex = 17;
@@ -120,15 +132,17 @@
             this.DisconnectButton.TabIndex = 18;
             this.DisconnectButton.Text = "Desconectar";
             this.DisconnectButton.UseVisualStyleBackColor = true;
+            this.DisconnectButton.Click += new System.EventHandler(this.DisconnectButton_Click);
             // 
-            // ConnectionButton
+            // ConnectButton
             // 
-            this.ConnectionButton.Location = new System.Drawing.Point(94, 113);
-            this.ConnectionButton.Name = "ConnectionButton";
-            this.ConnectionButton.Size = new System.Drawing.Size(75, 23);
-            this.ConnectionButton.TabIndex = 17;
-            this.ConnectionButton.Text = "Conectar";
-            this.ConnectionButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Location = new System.Drawing.Point(94, 113);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(75, 23);
+            this.ConnectButton.TabIndex = 17;
+            this.ConnectButton.Text = "Conectar";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectionButton_Click);
             // 
             // label3
             // 
@@ -165,11 +179,72 @@
             this.UserBox.TabIndex = 13;
             this.UserBox.Text = "sa";
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.DatabaseCombo);
+            this.groupBox1.Location = new System.Drawing.Point(318, 72);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(450, 47);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Base de datos";
+            // 
+            // DatabaseCombo
+            // 
+            this.DatabaseCombo.Enabled = false;
+            this.DatabaseCombo.FormattingEnabled = true;
+            this.DatabaseCombo.Location = new System.Drawing.Point(11, 19);
+            this.DatabaseCombo.Name = "DatabaseCombo";
+            this.DatabaseCombo.Size = new System.Drawing.Size(200, 21);
+            this.DatabaseCombo.TabIndex = 12;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.ColumnGrid);
+            this.groupBox3.Location = new System.Drawing.Point(318, 130);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(450, 229);
+            this.groupBox3.TabIndex = 19;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Tablas";
+            // 
+            // ColumnGrid
+            // 
+            this.ColumnGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ColumnGrid.Location = new System.Drawing.Point(12, 19);
+            this.ColumnGrid.Name = "ColumnGrid";
+            this.ColumnGrid.Size = new System.Drawing.Size(425, 201);
+            this.ColumnGrid.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(54, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 20);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Generar stored procedures";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(16, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 23;
+            this.pictureBox1.TabStop = false;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(835, 474);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.StatusBar);
             this.Controls.Add(this.MenuBar);
@@ -180,6 +255,10 @@
             this.MenuBar.PerformLayout();
             this.groupBox4.ResumeLayout(false);
             this.groupBox4.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ColumnGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -188,17 +267,23 @@
         #endregion
 
         private System.Windows.Forms.MenuStrip MenuBar;
-        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem FileMenu;
+        private System.Windows.Forms.ToolStripMenuItem FileExitMenu;
         private System.Windows.Forms.StatusStrip StatusBar;
         private System.Windows.Forms.GroupBox groupBox4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox ServerBox;
         private System.Windows.Forms.Button DisconnectButton;
-        private System.Windows.Forms.Button ConnectionButton;
+        private System.Windows.Forms.Button ConnectButton;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox PasswordBox;
         private System.Windows.Forms.TextBox UserBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox DatabaseCombo;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.DataGridView ColumnGrid;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- Generator.Win/MainForm.cs
+++ Generator.Win/MainForm.cs
@@ -11,9 +11,53 @@ namespace Generator.Win
 {
     public partial class MainForm : Form
     {
-        public MainForm()
-        {
-            InitializeComponent();
-        }
+        #region "Events"
+            private void ConnectionButton_Click(object sender, EventArgs e)
+            {
+                ConnectDatabase();
+            }
+
+            private void DisconnectButton_Click(object sender, EventArgs e)
+            {
+                DisconnectDatabase();
+            }
+
+            private void FileExitMenu_Click(object sender, EventArgs e)
+            {
+                Application.Exit();
+            }
+
+            public MainForm()
+            {
+                InitializeComponent();
+            }
+        #endregion
+
+        #region "Methods"
+            private void ConnectDatabase()
+            {
+                DisableControls();
+            }
+
+            private void DisableControls()
+            {
+                ServerBox.Enabled = false;
+                UserBox.Enabled = false;
+                PasswordBox.Enabled = false;
+                ConnectButton.Enabled = false;
+                DisconnectButton.Enabled = true;
+                DatabaseCombo.Enabled = true;
+            }
+
+            private void DisconnectDatabase()
+            {
+                ServerBox.Enabled = true;
+                UserBox.Enabled = true;
+                PasswordBox.Enabled = true;
+                ConnectButton.Enabled = true;
+                DisconnectButton.Enabled = false;
+                DatabaseCombo.Enabled = false;
+            }
+        #endregion
     }
 }
