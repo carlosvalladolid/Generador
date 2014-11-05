39d4c889be4705c58a61d2b42d94a9c4cb104669
carlosvalladolid <lachoman@hotmail.com>
2014-11-04 20:17:18 -0600

    Funcionalidad para mostrar bases de datos

--- Generator.Win/MainForm.cs
+++ Generator.Win/MainForm.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -11,6 +13,8 @@ namespace Generator.Win
 {
     public partial class MainForm : Form
     {
+        private int _ErrorId = 0;
+
         #region "Events"
             private void ConnectionButton_Click(object sender, EventArgs e)
             {
@@ -36,6 +40,7 @@ namespace Generator.Win
         #region "Methods"
             private void ConnectDatabase()
             {
+                OpenConnection();
                 DisableControls();
             }
 
@@ -57,6 +62,67 @@ namespace Generator.Win
                 ConnectButton.Enabled = true;
                 DisconnectButton.Enabled = false;
                 DatabaseCombo.Enabled = false;
+            }
+
+            public void GetTables(string Provider)
+            {
+                DataTable Tables;
+
+                using (SqlConnection SqlServer = new SqlConnection(Provider))
+                {
+                    //SqlServer.Open();
+
+                    //Tables = SqlServer.GetSchema("Tables");
+
+                    //SqlServer.Close();
+
+                    //if (Tables.Rows.Count > 0)
+                    //{
+                    //    // Muestra las tablas de la base de datos seleccionada en el combo
+                    //    Tables.DataSource = Tables;
+                    //    Tables.DisplayMember = "TABLE_NAME";
+                    //    Tables.ValueMember = "TABLE_NAME";
+                    //}
+                    //else
+                    //{
+                    //    Tables.DataSource = null;
+                    //}
+                }
+            }
+
+            private void OpenConnection()
+            {
+                string Provider = "";
+                DataTable Databases;
+
+                Provider = "Integrated Security=False; Persist Security Info=False; Server=" + ServerBox.Text.Trim() + "; ";
+                Provider += "User=" + UserBox.Text.Trim() + "; ";
+                Provider += "Password=" + PasswordBox.Text.Trim() + "; ";
+
+                using (SqlConnection SqlServer = new SqlConnection(Provider))
+                {
+                    SqlServer.Open();
+
+                    Databases = SqlServer.GetSchema("Databases");
+
+                    SqlServer.Close();
+
+                    if (Databases.Rows.Count > 0)
+                    {
+                        // Muestra las bases de datos del servidor en un comobobox
+                        DatabaseCombo.DataSource = Databases;
+                        DatabaseCombo.DisplayMember = "database_name";
+                        DatabaseCombo.ValueMember = "database_name";
+
+                        Provider = Provider + "Initial Catalog=" + Databases.Rows[0]["database_name"].ToString() + "; ";
+
+                        GetTables(Provider);
+                    }
+                    else
+                    {
+                        DatabaseCombo.DataSource = null;
+                    }
+                }
             }
         #endregion
     }
